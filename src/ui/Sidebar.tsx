c78cac5caf0e0250bf74b84835eced2497c98d38
Umut Taşkıran <umut.taskiran7@gmail.com>
2024-01-18 19:11:02 +0300

    add MainLogo and Navbar into Sidebar

--- src/ui/Sidebar.tsx
+++ src/ui/Sidebar.tsx
@@ -1,5 +1,13 @@
+import MainLogo from '@/ui/MainLogo';
+import Navbar from '@/ui/Navbar';
+
 function Sidebar() {
-  return <aside className="row-span-2 row-start-1">Sidebar</aside>;
+  return (
+    <aside className="row-span-2 row-start-1 flex flex-col gap-8 border-r border-gray-100 bg-white px-3 py-4">
+      <MainLogo />
+      <Navbar />
+    </aside>
+  );
 }
 
 export default Sidebar;
