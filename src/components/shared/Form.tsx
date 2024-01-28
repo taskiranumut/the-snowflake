baaaaa6e9d8c1134c2f98537d792cf8f414c1ff1
Umut Taşkıran <umut.taskiran7@gmail.com>
2024-01-28 23:42:19 +0300

    fix Form component type

--- src/components/shared/Form.tsx
+++ src/components/shared/Form.tsx
@@ -1,16 +1,20 @@
-import { type ReactNode } from 'react';
+import { ComponentPropsWithoutRef, type ReactNode } from 'react';
 
 type FormProps = {
   children: ReactNode;
   modal?: boolean | undefined;
-};
+} & ComponentPropsWithoutRef<'form'>;
 
-function Form({ children, modal }: FormProps) {
+function Form({ children, modal, ...props }: FormProps) {
   const baseStyle = 'overflow-hidden text-base';
   const modalFormStyle = `${baseStyle} w-[54rem]`;
   const formStyle = `${baseStyle} px-3 py-2 bg-white border rounded-md`;
 
-  return <form className={modal ? modalFormStyle : formStyle}>{children}</form>;
+  return (
+    <form className={modal ? modalFormStyle : formStyle} {...props}>
+      {children}
+    </form>
+  );
 }
 
 export default Form;
