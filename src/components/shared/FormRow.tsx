88a8e121cc9a220b56115b4d4680f006e5caaa43
Umut Taşkıran <umut.taskiran7@gmail.com>
2024-01-29 16:46:48 +0300

    add error option to FormRow

--- src/components/shared/FormRow.tsx
+++ src/components/shared/FormRow.tsx
@@ -2,12 +2,14 @@ import { type ReactNode } from 'react';
 
 type FormRowProps = {
   children: ReactNode;
+  error?: string;
 };
 
-function FormRow({ children }: FormRowProps) {
+function FormRow({ error, children }: FormRowProps) {
   return (
     <div className="grid grid-cols-[16rem_1fr_1.2fr] items-center gap-6 border-b border-b-gray-100 px-0 py-3 first:pt-0 last:border-0 last:pb-0 has-[button]:flex has-[button]:justify-end has-[button]:gap-3">
       {children}
+      {error && <div className="text-base text-red-700">{error}</div>}
     </div>
   );
 }
