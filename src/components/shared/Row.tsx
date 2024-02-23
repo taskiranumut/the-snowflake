2c2199fec0d8cc7104bbc860484dce64682656d3
Umut Taşkıran <umut.taskiran7@gmail.com>
2024-02-23 11:56:45 +0300

    extend Row component with div props

--- src/components/shared/Row.tsx
+++ src/components/shared/Row.tsx
@@ -1,17 +1,26 @@
-import { ReactNode } from 'react';
+import { type ComponentPropsWithoutRef, type ReactNode } from 'react';
 
 type RowProps = {
   type?: 'vertical' | 'horizontal';
   children: ReactNode;
-};
+} & ComponentPropsWithoutRef<'div'>;
 
-function Row({ type = 'vertical', children }: RowProps) {
+function Row({
+  type = 'vertical',
+  className,
+  children = '',
+  ...otherProps
+}: RowProps) {
   const typeStyleMap = {
     vertical: 'flex flex-col gap-4',
     horizontal: 'flex items-center justify-between',
   };
 
-  return <div className={typeStyleMap[type]}>{children}</div>;
+  return (
+    <div className={`${typeStyleMap[type]} ${className}`} {...otherProps}>
+      {children}
+    </div>
+  );
 }
 
 export default Row;
