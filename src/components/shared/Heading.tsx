a6dd4d08b285b2f6d52a379a037c58e81cc0c48e
Umut Taşkıran <umut.taskiran7@gmail.com>
2024-02-24 19:38:52 +0300

    fix default props issue for Heading component

--- src/components/shared/Heading.tsx
+++ src/components/shared/Heading.tsx
@@ -1,15 +1,37 @@
-import { type ReactNode } from 'react';
+import { type ComponentPropsWithoutRef, type ReactNode } from 'react';
 
 type HeadingProps = {
   children: ReactNode;
   as: 'h1' | 'h2' | 'h3';
-};
+} & (
+  | ComponentPropsWithoutRef<'h1'>
+  | ComponentPropsWithoutRef<'h2'>
+  | ComponentPropsWithoutRef<'h3'>
+);
 
-function Heading({ as, children }: HeadingProps) {
+function Heading({ as, children, className, ...otherProps }: HeadingProps) {
   const headingMap = {
-    h1: <h1 className="text-3xl font-semibold leading-6">{children}</h1>,
-    h2: <h2 className="text-xl font-semibold leading-6">{children}</h2>,
-    h3: <h3 className="text-xl leading-6">{children}</h3>,
+    h1: (
+      <h1
+        className={`text-3xl font-semibold leading-6 ${className}`}
+        {...otherProps}
+      >
+        {children}
+      </h1>
+    ),
+    h2: (
+      <h2
+        className={`text-xl font-semibold leading-6 ${className}`}
+        {...otherProps}
+      >
+        {children}
+      </h2>
+    ),
+    h3: (
+      <h3 className={`text-xl leading-6 ${className}`} {...otherProps}>
+        {children}
+      </h3>
+    ),
   };
 
   return headingMap[as];
