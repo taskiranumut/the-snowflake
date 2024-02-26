bccbfe68882477af8022fc9771ce7f1a3c311fa6
Umut Taşkıran <umut.taskiran7@gmail.com>
2024-02-26 12:18:02 +0300

    fix modal css classnames

--- src/components/shared/Modal.tsx
+++ src/components/shared/Modal.tsx
@@ -13,7 +13,7 @@ function Modal({ open, onClose, children }: ModalProps) {
     open &&
     createPortal(
       <div className="fixed left-0 top-0 z-50 h-screen w-full bg-gray-500 bg-opacity-50 backdrop-blur-sm transition-all duration-500">
-        <div className="fixed left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] rounded-md bg-white p-4 shadow-md transition duration-500">
+        <div className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 rounded-md bg-white p-4 shadow-md transition duration-500">
           <button
             className="absolute right-4 top-3 translate-x-2 rounded-md border-0 bg-none p-2 outline-none transition-all duration-200 hover:bg-gray-100 focus:border-0 focus:outline-none focus:ring-2 focus:ring-sky-300"
             type="button"
