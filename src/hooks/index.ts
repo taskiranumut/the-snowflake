040a6f7028612e15be6c13eb9b06c04c0e6b9efa
Umut Taşkıran <umut.taskiran7@gmail.com>
2024-03-14 01:18:19 +0300

    add useLocalStorageState hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,4 @@
 export { useDebounce } from '@/hooks/useDebounce';
 export { useOutsideClick } from '@/hooks/useOutsideClick';
 export { useMoveBack } from '@/hooks/useMoveBack';
+export { useLocalStorageState } from '@/hooks/useLocalStorageState';
