f48db69f65be0dda8bc1e1d4893624fa2fef5fe4
Umut Taşkıran <umut.taskiran7@gmail.com>
2024-03-15 22:23:26 +0300

    add route direction to MainLogo

--- src/components/MainLogo.tsx
+++ src/components/MainLogo.tsx
@@ -1,10 +1,20 @@
-export function MainLogo() {
+import { useNavigate } from 'react-router-dom';
+import { twMerge } from 'tailwind-merge';
+
+type MainLogoProps = {
+  redirect?: boolean;
+};
+
+export function MainLogo({ redirect = false }: MainLogoProps) {
+  const navigate = useNavigate();
+
   return (
     <div className="flex flex-col items-center justify-start gap-1">
       <img
-        className="h-32 w-auto"
+        className={twMerge('h-32 w-auto', redirect ? 'cursor-pointer' : '')}
         src="/hotel-logo.png"
         alt="The Snowflake Logo"
+        onClick={() => redirect && navigate('/dashboard')}
       />
       <h1 className="text-xl text-gray-600">The Snowflake</h1>
     </div>
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -4,7 +4,7 @@ import { Uploader } from '@/data/Uploader';
 export function Sidebar() {
   return (
     <aside className="row-span-2 row-start-1 flex flex-col gap-8 border-r border-gray-100 bg-white px-3 py-4">
-      <MainLogo />
+      <MainLogo redirect />
       <Navbar />
       <Uploader />
     </aside>
