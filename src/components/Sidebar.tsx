b031659493f7e42bcb68e243c70dcf21075491ed
Umut Taşkıran <umut.taskiran7@gmail.com>
2024-03-15 23:05:31 +0300

    fix Sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,12 +1,10 @@
 import { MainLogo, Navbar } from '@/components';
-import { Uploader } from '@/data/Uploader';
 
 export function Sidebar() {
   return (
     <aside className="row-span-2 row-start-1 flex flex-col gap-8 border-r border-gray-100 bg-white px-3 py-4">
       <MainLogo redirect />
       <Navbar />
-      <Uploader />
     </aside>
   );
 }
