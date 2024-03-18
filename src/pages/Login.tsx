3a06e716f51b15b4cf12691fd6385abe83790ff3
Umut Taşkıran <umut.taskiran7@gmail.com>
2024-03-19 00:27:16 +0300

    fix entering the login screen while logged in

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,15 +1,33 @@
 import { MainLogo } from '@/components';
-import { Heading } from '@/components/shared';
+import { Heading, Spinner } from '@/components/shared';
 import { LoginForm } from '@/features/auth/components';
+import { useUser } from '@/features/auth/hooks';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export function Login() {
+  const { isLoading, isAuth } = useUser();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isAuth) {
+      navigate('/dashboard');
+    }
+  }, [isAuth, navigate]);
+
   return (
     <div className="grid min-h-screen grid-cols-[32rem] content-center justify-center gap-8 bg-gray-50 dark:bg-gray-900">
-      <MainLogo />
-      <Heading as="h4" className="text-center">
-        Log in to your account
-      </Heading>
-      <LoginForm />
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <>
+          <MainLogo />
+          <Heading as="h4" className="text-center">
+            Log in to your account
+          </Heading>
+          <LoginForm />
+        </>
+      )}
     </div>
   );
 }
