ae579ce1f383f7758a899e455403e080b2ab0df0
Umut Taşkıran <umut.taskiran7@gmail.com>
2024-03-18 16:43:25 +0300

    refactor NavItem to use twMerge

--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -1,5 +1,6 @@
 import { NavLink } from 'react-router-dom';
 import { type ReactNode } from 'react';
+import { twMerge } from 'tailwind-merge';
 
 type NavItemProps = {
   to: string;
@@ -9,23 +10,32 @@ type NavItemProps = {
 };
 
 export function NavItem({ to, icon, title, children }: NavItemProps) {
-  const baseLink =
-    'flex items-center gap-2 px-4 py-3 text-base font-normal hover:rounded-md hover:bg-gray-100 dark:hover:bg-gray-900 hover:text-gray-800 dark:hover:text-gray-100 hover:transition-colors hover:duration-200';
-  const activeLink = baseLink + ' rounded-md bg-gray-100 dark:bg-gray-900';
-  const passiveLink = baseLink + ' text-gray-600 dark:text-gray-300';
+  const activeLink = 'rounded-md bg-gray-100 dark:bg-gray-900';
+  const passiveLink = 'text-gray-600 dark:text-gray-300';
 
-  const baseIcon = 'text-xl';
-  const activeIcon = baseIcon + ' text-emerald-600';
-  const passiveIcon = baseIcon + ' text-gray-400 dark:text-gray-500';
+  const activeIcon = 'text-emerald-600';
+  const passiveIcon = 'text-gray-400 dark:text-gray-500';
 
   return (
     <NavLink
       to={to}
-      className={({ isActive }) => (isActive ? activeLink : passiveLink)}
+      className={({ isActive }) =>
+        twMerge(
+          'flex items-center gap-2 px-4 py-3 text-base font-normal hover:rounded-md hover:bg-gray-100 hover:text-gray-800 hover:transition-colors hover:duration-200 dark:hover:bg-gray-900 dark:hover:text-gray-100',
+          isActive ? activeLink : passiveLink,
+        ) as string
+      }
       children={({ isActive }) => (
         <>
           {icon && (
-            <span className={isActive ? activeIcon : passiveIcon}>{icon}</span>
+            <span
+              className={twMerge(
+                'text-xl',
+                isActive ? activeIcon : passiveIcon,
+              )}
+            >
+              {icon}
+            </span>
           )}
           {title && <span>{title}</span>}
           {children}
