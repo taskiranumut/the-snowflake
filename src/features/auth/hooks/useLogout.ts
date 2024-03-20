99b1720fa6fbe3b8353e72b6f5d72a5008a9286b
Umut Taşkıran <umut.taskiran7@gmail.com>
2024-03-20 19:59:11 +0300

    close sidebar after logout

--- src/features/auth/hooks/useLogout.ts
+++ src/features/auth/hooks/useLogout.ts
@@ -1,3 +1,4 @@
+import { useToggleSidebarContext } from '@/context';
 import { logout } from '@/services/api';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import toast from 'react-hot-toast';
@@ -6,12 +7,14 @@ import { useNavigate } from 'react-router-dom';
 export function useLogout() {
   const navigate = useNavigate();
   const queryClient = useQueryClient();
+  const { onClose } = useToggleSidebarContext();
 
   const { isPending: isLoading, mutate: mutateLogout } = useMutation({
     mutationFn: logout,
     onSuccess: () => {
       queryClient.removeQueries();
       navigate('/login', { replace: true });
+      onClose();
     },
     onError: () => {
       toast.error('There was an error. Please try again a few minutes later.');
