2918bce2a3bb0f50bb114afe4618f263d639ef5e
Umut Taşkıran <umut.taskiran7@gmail.com>
2024-03-20 11:54:13 +0300

    add optional prop to Row for responsiveness

--- src/components/shared/Row.tsx
+++ src/components/shared/Row.tsx
@@ -2,19 +2,21 @@ import { type ComponentPropsWithoutRef, type ReactNode } from 'react';
 import { twMerge } from 'tailwind-merge';
 
 type RowProps = {
-  type?: 'vertical' | 'horizontal';
   children: ReactNode;
+  type?: 'vertical' | 'horizontal';
+  responsive?: boolean;
 } & ComponentPropsWithoutRef<'div'>;
 
 export function Row({
   type = 'vertical',
   className = '',
   children = '',
+  responsive = false,
   ...otherProps
 }: RowProps) {
   const typeStyleMap = {
     vertical: 'flex flex-col gap-4',
-    horizontal: 'flex items-center justify-between',
+    horizontal: `${responsive ? 'flex flex-col sm:flex-row gap-4 sm:gap-0 items-start sm:items-center justify-center sm:justify-between' : 'flex items-center justify-between'}`,
   };
 
   return (
