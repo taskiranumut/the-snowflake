b78358c3c159f27415c5aa614ebceadc79c7abad
Umut Taşkıran <umut.taskiran7@gmail.com>
2024-03-20 20:04:33 +0300

    fix toggle sidebar action after logout

--- src/features/auth/hooks/useLogout.ts
+++ src/features/auth/hooks/useLogout.ts
@@ -1,20 +1,23 @@
 import { useToggleSidebarContext } from '@/context';
+import { useIsMobileDevice } from '@/hooks';
 import { logout } from '@/services/api';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import toast from 'react-hot-toast';
 import { useNavigate } from 'react-router-dom';
 
 export function useLogout() {
+  const { onClose, onOpen } = useToggleSidebarContext();
+  const isMobileDevice = useIsMobileDevice();
+
   const navigate = useNavigate();
   const queryClient = useQueryClient();
-  const { onClose } = useToggleSidebarContext();
 
   const { isPending: isLoading, mutate: mutateLogout } = useMutation({
     mutationFn: logout,
     onSuccess: () => {
       queryClient.removeQueries();
       navigate('/login', { replace: true });
-      onClose();
+      isMobileDevice ? onClose() : onOpen();
     },
     onError: () => {
       toast.error('There was an error. Please try again a few minutes later.');
