ca8311e4901682101189ffb90cff52965fb3e31b
Umut Taşkıran <umut.taskiran7@gmail.com>
2024-04-03 22:37:45 +0300

    fix the issue where toaster color does not change when changing theme

--- src/providers/ToasterProvider.tsx
+++ src/providers/ToasterProvider.tsx
@@ -1,6 +1,10 @@
 import { Toaster } from 'react-hot-toast';
+import { useThemeContext } from '@/context';
+import { twMerge } from 'tailwind-merge';
 
 export function ToasterProvider() {
+  const { theme } = useThemeContext();
+
   return (
     <Toaster
       position="top-center"
@@ -9,8 +13,12 @@ export function ToasterProvider() {
       toastOptions={{
         success: { duration: 3000 },
         error: { duration: 5000 },
-        className:
-          'text-base max-w-[500px] px-4 py-3 bg-white dark:bg-gray-700 text-gray-700 dark:text-gray-200',
+        className: twMerge(
+          'text-base max-w-[500px] px-4 py-3',
+          theme === 'light'
+            ? 'bg-white text-gray-700'
+            : 'bg-gray-700 text-gray-200',
+        ),
       }}
     />
   );
