7c746fd7fd35da7d2d74f665a3048ecc0419eedc
Umut Taşkıran <umut.taskiran7@gmail.com>
2024-04-03 22:43:55 +0300

    fix toaster issue

--- src/providers/ToasterProvider.tsx
+++ src/providers/ToasterProvider.tsx
@@ -1,6 +1,5 @@
 import { Toaster } from 'react-hot-toast';
 import { useThemeContext } from '@/context';
-import { twMerge } from 'tailwind-merge';
 
 export function ToasterProvider() {
   const { theme } = useThemeContext();
@@ -13,12 +12,11 @@ export function ToasterProvider() {
       toastOptions={{
         success: { duration: 3000 },
         error: { duration: 5000 },
-        className: twMerge(
-          'text-base max-w-[500px] px-4 py-3',
-          theme === 'light'
-            ? 'bg-white text-gray-700'
-            : 'bg-gray-700 text-gray-200',
-        ),
+        className: 'text-base max-w-[500px] px-4 py-3',
+        style: {
+          backgroundColor: theme === 'light' ? '#FFFFFF' : '#374151',
+          color: theme === 'light' ? '#374151' : '#e5e7eb',
+        },
       }}
     />
   );
