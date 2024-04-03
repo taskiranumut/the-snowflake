3869183c96f7b866d91c96323ad48a4d68a62707
Umut Taşkıran <umut.taskiran7@gmail.com>
2024-04-03 22:26:22 +0300

    fix dashbord filter responsive bug

--- src/features/dashboard/components/DashboardOperations.tsx
+++ src/features/dashboard/components/DashboardOperations.tsx
@@ -5,13 +5,15 @@ export function DashboardOperations() {
   const { t } = useTranslation();
 
   return (
-    <Filter
-      queryField="last"
-      options={[
-        { value: '7', label: t('label.dashboard.filter.last7') },
-        { value: '30', label: t('label.dashboard.filter.last30') },
-        { value: '90', label: t('label.dashboard.filter.last90') },
-      ]}
-    />
+    <div className="w-full md:w-60 lg:w-auto">
+      <Filter
+        queryField="last"
+        options={[
+          { value: '7', label: t('label.dashboard.filter.last7') },
+          { value: '30', label: t('label.dashboard.filter.last30') },
+          { value: '90', label: t('label.dashboard.filter.last90') },
+        ]}
+      />
+    </div>
   );
 }
